ad08baa6ee3aaad74c844546d61dc1b79bb29131
Pavel.Liakhouski <pavel.liakhouski@gds.ey.com>
2026-06-20 14:45:00 +0200

    feat(cart): add Beanstalk support [cart-high]
    Signed-off-by: Pavel.Liakhouski <pavel.liakhouski@gds.ey.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://g2nxv4ryxj.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://a6ehcn2l33.execute-api.us-east-1.amazonaws.com/prod",
+  order: "https://d385u818c8s4y6.cloudfront.net",
   import: "https://noo80oqze7.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://a6ehcn2l33.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://a6ehcn2l33.execute-api.us-east-1.amazonaws.com/prod",
+  cart: "https://d385u818c8s4y6.cloudfront.net",
 };
 
 export default API_PATHS;
